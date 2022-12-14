e025e7d62a3e5c50fcfa13b3f602a2c386ea1714
sanjithacks <sanjithacks@gmail.com>
2022-12-14 14:42:17 +0530

    redesigned

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const image = document.querySelector('.image');
-const hover = document.querySelector('.hover');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.close');
+const image = document.querySelector(".image");
+const hover = document.querySelector(".hover");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+const shareBtn = document.querySelector(".share");
 
-function show(){
-    hover.classList.add('active');
-    modal.classList.add('show');
+function show() {
+  hover.classList.add("active");
+  modal.classList.add("show");
 }
 
-function hide(){
-    hover.classList.remove('active');
-    modal.classList.remove('show');
+function hide() {
+  hover.classList.remove("active");
+  modal.classList.remove("show");
 }
 
-image.addEventListener('click', show);
-close.addEventListener('click', hide);
+image.addEventListener("click", show);
+close.addEventListener("click", hide);
+
+let shareData = {
+  title: document.title,
+  text: `Look what I found ${document.title}`,
+  url: window.location.href,
+};
+
+const btn = document.querySelector("button");
+const resultPara = document.querySelector(".result");
+
+shareBtn.addEventListener("click", () => {
+  navigator
+    .share(shareData)
+    .then(() => (resultPara.textContent = "MDN shared successfully"))
+    .catch((e) => (resultPara.textContent = "Error: " + e));
+});
